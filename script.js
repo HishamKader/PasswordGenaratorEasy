691577befdce488911ca6ea39b838bf2bf0b8581
Hisham Kader <hisham319@hotmail.com>
2020-10-02 19:47:27 -0400

    added functionality to the slider on the password generator

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-// var specialChar = ["!", "@","#","$","%","^","&","*","-","_","=","+"];
-// var upperCase = ["A","B","C","D","E","F","G",];
-// var lowerCase = [];
-// var numeric = [];
-
-// function password() {
-//     var passLength = prompt("How long do you want your password");
-
-//     if (isNaN(passLength) === true) {
-//         alert("This is not a number");
-//         return;
-//     }
-
-//     if (passLength < 8 && passLength > 128) {
-//         alert("Atleast 8 to 128 characters in the password");
-//         return;
-//     }
-
-//     var specialCharacters = confirm("Must contain special characters");
-//     var upperCaseChar = confirm("")
-//     var lowerCaseChar = confirm("")
-//     var numericChar = confirm("")
-
-//     if (specialCharacters === false && upperCaseChar === false && lowerCaseChar === false && numericChar === false) {
-//         alert("Must select at least one character type")
-//         return;
-//     }
-
-
-// }
 //generate random password
 function generate(){
 
@@ -47,10 +17,16 @@ function generate(){
     //add password to textbox/display area
     document.getElementById("display").value = password;
 
-    
-
+    // add password to previously generated passwords section
+    document.getElementById("lastNums").innerHTML
 
 }
+
+// set defualt length display of 25
+document.getElementById("length").innerHTML = "length: 8";
+
+// function to set length based on slider position
+document.getElementById("slider").oninput = function()
 
 
 

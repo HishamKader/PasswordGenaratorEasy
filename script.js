291a1b55aa4fc41b7c7d525207dc1ba649017dc7
Hisham Kader <hisham319@hotmail.com>
2020-10-02 19:34:01 -0400

    Added functionality to the genarate button from the script.js file, and html was completed

--- script.js
+++ script.js
@@ -28,22 +28,29 @@
 
 
 // }
+//generate random password
+function generate(){
 
-function generate()
+    //set password length/complexity
+    let complexity = document.getElementById("slider").value;
 
-// set password length/complexity
-let complexity = document.getElementById("slider").value;
+    //possible password values
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 
-// possible password values
-let values = "ABCDEFGHIJLKMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+    let password = "";
 
-// create for loop to choose password characters
-for(i = 0; i<= complexity; i++){
-    password = password = values.charAt(Math.floor(Math.random)() * Math.floor(values.length - 1));
+    //create for loop to choose password characters
+    for(var i = 0; i <= complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    }
+
+    //add password to textbox/display area
+    document.getElementById("display").value = password;
+
+    
+
+
 }
-
-
-
 
 
 
